90549011e4c4276477f42564278acb7b7a161093
Civic User <user@example.com>
2026-04-23 21:10:54 -0700

    Delete Firebase files physically

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
 };
 
 export default nextConfig;
-
